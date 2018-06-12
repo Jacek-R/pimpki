66b129bd53a1283a96708a17388a4f6dff6dd4f8
Jacek <jacek.ragann@gmail.com>
2018-06-12 11:33:27 +0200

    Create CellView model with graphic representation of cell

---
+++ src/main/java/model/cell/CellView.java
@@ -0,0 +1,31 @@
+package model.cell;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+class CellView {
+
+    private StackPane stackPane;
+    private ImageView background;
+    private ImageView content;
+
+    public CellView(StackPane stackPane, ImageView background, ImageView content) {
+        this.stackPane = stackPane;
+        this.background = background;
+        this.content = content;
+        stackPane.getChildren().add(background);
+        stackPane.getChildren().add(content);
+    }
+
+    public StackPane getStackPane() {
+        return stackPane;
+    }
+
+    public void setBackground(ImageView background) {
+        this.background = background;
+    }
+
+    public void setContent(ImageView content) {
+        this.content = content;
+    }
+}
