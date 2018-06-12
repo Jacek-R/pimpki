e26b2385d542c242dad9c045501e889db79ed097
Jacek <jacek.ragann@gmail.com>
2018-06-12 15:45:37 +0200

    Create class which contains world

---
+++ src/main/java/model/World.java
@@ -0,0 +1,33 @@
+package model;
+
+import javafx.scene.layout.GridPane;
+import model.cell.Cell;
+
+public class World {
+
+    private int width;
+    private int height;
+
+    private Cell[][] cells;
+    private GridPane gridPane;
+
+    public World(int width, int height, Cell[][] cells) {
+        this.width = width;
+        this.height = height;
+        this.cells = cells;
+        createView();
+    }
+
+    private void createView() {
+        gridPane = new GridPane();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                gridPane.add(cells[x][y].getCellView().getStackPane(), x, y);
+            }
+        }
+    }
+
+    public GridPane getGridPane() {
+        return gridPane;
+    }
+}
