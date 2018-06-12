cb0169e87faf9eb8d5b0644f575a721955262fc9
Jacek <jacek.ragann@gmail.com>
2018-06-12 11:50:03 +0200

    Create basic cell and move to cell package

---
+++ src/main/java/model/cell/Cell.java
@@ -0,0 +1,26 @@
+package model.cell;
+
+import model.Content;
+
+public class Cell {
+
+    private CellView cellView;
+    private Content content;
+
+    public Cell(CellView cellView, Content content) {
+        this.cellView = cellView;
+        this.content = content;
+    }
+
+    public CellView getCellView() {
+        return cellView;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+}
