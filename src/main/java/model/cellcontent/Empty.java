23e234bd3838f7a7ffae8a35e05a8ea69a0a3531
Jacek <jacek.ragann@gmail.com>
2018-06-13 15:56:47 +0200

    Implement getType and isAccessible methods

--- src/main/java/model/cellcontent/Empty.java
+++ src/main/java/model/cellcontent/Empty.java
@@ -7,10 +7,22 @@ import java.io.FileNotFoundException;
 
 public class Empty implements Content {
     private static final String IMAGE_PATH = "src/main/resources/img/empty.png";
-    private boolean accessible = true;
+
+    private static final Type TYPE = Type.EMPTY;
+    private static final boolean ACCESSIBLE = true;
 
     @Override
     public Image getImage() throws FileNotFoundException {
         return new Image(new FileInputStream(IMAGE_PATH));
+    }
+
+    @Override
+    public Type getType() {
+        return TYPE;
+    }
+
+    @Override
+    public boolean isAccessible() {
+        return ACCESSIBLE;
     }
 }
