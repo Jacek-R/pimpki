673b795fb3ae76e740a4ded32598f169260f8438
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-14 00:03:24 +0200

    Add implementation of spawn method in foodspawner

--- src/main/java/model/foodSpawner/BasicFoodSpawner.java
+++ src/main/java/model/foodSpawner/BasicFoodSpawner.java
@@ -1,27 +1,38 @@
 package model.foodSpawner;
 
 import explorer.WorldManager;
+import model.coordinates.Coordinates;
 import model.food.Food;
+import world.Board;
 
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 public class BasicFoodSpawner implements FoodSpawner {
 
     private WorldManager worldManager;
+    private Board board;
+    private int width;
+    private int height;
 
     public BasicFoodSpawner(WorldManager worldManager) {
         this.worldManager = worldManager;
     }
 
     @Override
-    public boolean spawn(Set<Food> foodCollection) {
-
-
-        // algorytm wybiera coordynaty (Coordinates)
-        // po wybraniu rejetrujemy w worldManager:
-        // worldManager.registerFood(food); <-- ta metoda (worldManagera) mogłaby również seterem ustawić
-        // Content w danym cell
-
+    public boolean spawn(Set<Food> foodCollection) throws FileNotFoundException {
+        this.board = worldManager.getBoard();
+        width = board.getWidth();
+        height = board.getHeight();
+        for (int i = 0; i < foodCollection.size(); i++) {
+            boolean contentPlaced = false;
+            while (!contentPlaced){
+                Coordinates coordinates = worldManager.selectRandomCoordinates();
+                if (worldManager.isEmpty(coordinates)) {
+                    contentPlaced = worldManager.registerFood(coordinates, foodCollection.iterator().next());
+                }
+            }
+        }
         return true;
     }
 }
--- src/main/java/model/foodSpawner/FoodSpawner.java
+++ src/main/java/model/foodSpawner/FoodSpawner.java
@@ -2,9 +2,10 @@ package model.foodSpawner;
 
 import model.food.Food;
 
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 public interface FoodSpawner {
 
-    boolean spawn(Set<Food> foodCollection);
+    boolean spawn(Set<Food> foodCollection) throws FileNotFoundException;
 }
