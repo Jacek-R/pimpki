cd4ba848502bd4830615b5befda345b90ed1e51e
jarqprog <jarqprog@gmail.com>
2018-06-13 20:05:26 +0200

    change contructor in WorldManager

--- src/main/java/explorer/MapManager.java
+++ src/main/java/explorer/MapManager.java
@@ -15,14 +15,15 @@ import java.util.Map;
 
 public class MapManager implements WorldManager {
 
-    private final Board board;
+    private Board board;
 
     private Map<Coordinates,Predator> predators = new HashMap<>();
     private Map<Coordinates,Pacifist> pacifists = new HashMap<>();
     private Map<Coordinates,Food> food = new HashMap<>();
     private Map<Coordinates,Obstacle> obstacles = new HashMap<>();
 
-    public MapManager(Board board) {
+    @Override
+    public void setBoard(Board board) {
         this.board = board;
     }
 
