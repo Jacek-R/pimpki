deba56d38dfc9fb3bdc89fd3d70ea9a3ac0d6663
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-13 21:39:29 +0200

    Add ObstacleSpawner in Construcor of WorldCreator

--- src/main/java/world/WorldCreator.java
+++ src/main/java/world/WorldCreator.java
@@ -14,11 +14,13 @@ import model.cellcontent.Wall;
 import model.configuration.Configuration;
 import model.food.Food;
 import model.foodSpawner.FoodSpawner;
+import model.obstacleSpawner.ObstacleSpawner;
 import model.pimpek.Pimpek;
 import model.pimpekSpawner.PimpekSpawner;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -28,37 +30,43 @@ public class WorldCreator implements BoardCreator {
     private final Configuration configuration;
     private final FoodSpawner foodSpawner;
     private final PimpekSpawner pimpekSpawner;
+    private final ObstacleSpawner obstacleSpawner;
     private Set<Pimpek> beings;
     private Set<Food> supplies;
     private int height;
     private int width;
+    private int obstaclesQuantity;
 
 
     public WorldCreator(WorldManager explorer, Configuration configuration,
-                        FoodSpawner foodSpawner, PimpekSpawner pimpekSpawner, Set<Pimpek> beings, Set<Food>supplies) {
+                        FoodSpawner foodSpawner, PimpekSpawner pimpekSpawner,
+                        ObstacleSpawner obstacleSpawner, Set<Pimpek> beings, Set<Food> supplies) {
         this.explorer = explorer;
         this.configuration = configuration;
         this.foodSpawner = foodSpawner;
         this.pimpekSpawner = pimpekSpawner;
+        this.obstacleSpawner = obstacleSpawner;
         this.beings = beings;
         this.supplies = supplies;
         this.height = configuration.getMapHeight();
         this.width = configuration.getMapWidth();
+        this.obstaclesQuantity = configuration.getObstaclesQuantity();
     }
 
     @Override
     public Board create() throws FileNotFoundException{
         Board board = createEmptyBoard();
         explorer.setBoard(board);
-        Cell[][] cells = board.getCells();
-        populateWithElements(cells, Type.WALL);
-        board.setCells(cells);
+        populate();
         return board;
     }
 
     public Board createEmptyBoard() throws FileNotFoundException {
         Cell[][] cells = createEmptyCells();
         return new World(width, height, cells);
+    }
+
+    public void populate(){
     }
 
     private Cell[][] createEmptyCells() throws FileNotFoundException {
@@ -72,22 +80,6 @@ public class WorldCreator implements BoardCreator {
         return cells;
     }
 
-    private void populateWithElements(Cell[][] cells, Type type) throws FileNotFoundException {
-        int quantity = 10;
-        for (int i = 0; i < quantity; i++) {
-            boolean contentPlaced = false;
-            int triesLeft = width * height * 10;
-            do {
-                Cell cell = selectRandomCell(cells);
-                if (cell.getContent().getType() == Type.EMPTY) {
-                    placeContent(cell, type);
-                    contentPlaced = true;
-                }
-                triesLeft--;
-            } while (!contentPlaced && triesLeft > 0);
-        }
-    }
-
     private CellView createCellView(Content content) throws FileNotFoundException {
         return new CellView(
                 new StackPane(),
@@ -95,42 +87,58 @@ public class WorldCreator implements BoardCreator {
                 new ImageView(ImageParser.getImage(content.getImagePath()))
         );
     }
-
-    private Cell selectRandomCell(Cell[][] cells) {
-        Random random = new Random();
-        int x = random.nextInt(width);
-        int y = random.nextInt(height);
-        return cells[x][y];
-    }
-
-    private void placeContent(Cell cell, Type type) throws FileNotFoundException {
-        Content content = createContent(type);
-        cell.setContent(content);
-        cell.getCellView().setContent(ImageParser.getImage(content.getImagePath()));
-    }
-
-    private Content createContent(Type type) {
-        Content content;
-        switch (type) {
-            case EMPTY:
-                content = new Empty();
-                break;
-            case WALL:
-                content = new Wall();
-                break;
-            case PIMPEK:
-                content = new Empty();
-                break;
-            case PREDATOR:
-                content = new Empty();
-                break;
-            case FOOD:
-                content = new Empty();
-                break;
-            default:
-                content = new Empty();
-                break;
-        }
-        return content;
-    }
+//
+//    private void populateWithElements(Cell[][] cells, Type type) throws FileNotFoundException {
+//        int quantity = 10;
+//        for (int i = 0; i < quantity; i++) {
+//            boolean contentPlaced = false;
+//            int triesLeft = width * height * 10;
+//            do {
+//                Cell cell = selectRandomCell(cells);
+//                if (cell.getContent().getType() == Type.EMPTY) {
+//                    placeContent(cell, type);
+//                    contentPlaced = true;
+//                }
+//                triesLeft--;
+//            } while (!contentPlaced && triesLeft > 0);
+//        }
+//    }
+//
+//    private Cell selectRandomCell(Cell[][] cells) {
+//        Random random = new Random();
+//        int x = random.nextInt(width);
+//        int y = random.nextInt(height);
+//        return cells[x][y];
+//    }
+//
+//    private void placeContent(Cell cell, Type type) throws FileNotFoundException {
+//        Content content = createContent(type);
+//        cell.setContent(content);
+//        cell.getCellView().setContent(ImageParser.getImage(content.getImagePath()));
+//    }
+//
+//    private Content createContent(Type type) {
+//        Content content;
+//        switch (type) {
+//            case EMPTY:
+//                content = new Empty();
+//                break;
+//            case WALL:
+//                content = new Wall();
+//                break;
+//            case PIMPEK:
+//                content = new Empty();
+//                break;
+//            case PREDATOR:
+//                content = new Empty();
+//                break;
+//            case FOOD:
+//                content = new Empty();
+//                break;
+//            default:
+//                content = new Empty();
+//                break;
+//        }
+//        return content;
+//    }
 }
