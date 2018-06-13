ca3e82573e4e98e3d8b81c9b5e554edcc889ca8e
AdamKruk91 <adam.kruk91@gmail.com>
2018-06-14 00:18:17 +0200

    Implement spawn method in PimpekSpawner

--- src/main/java/model/pimpekSpawner/BasicPimpekSpawner.java
+++ src/main/java/model/pimpekSpawner/BasicPimpekSpawner.java
@@ -1,26 +1,34 @@
 package model.pimpekSpawner;
 
 import explorer.WorldManager;
+import model.coordinates.Coordinates;
 import model.pimpek.Pimpek;
+import world.Board;
+
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 public class BasicPimpekSpawner implements PimpekSpawner {
 
     private WorldManager worldManager;
+    private Board board;
 
     public BasicPimpekSpawner(WorldManager worldManager) {
         this.worldManager = worldManager;
     }
 
     @Override
-    public boolean spawn(Set<Pimpek> pimpki) {
-
-        // algorytm wybiera coordynaty (Coordinates)
-        // po wybraniu rejetrujemy w worldManager:
-        // worldManager.registerBeing(pimpek); <-- ta metoda (worldManagera) mogłaby również seterem ustawić
-        // Content w danym cell
-
-
+    public boolean spawn(Set<Pimpek> pimpki) throws FileNotFoundException {
+        this.board = worldManager.getBoard();
+        for (int i = 0; i < pimpki.size(); i++) {
+            boolean contentPlaced = false;
+            while (!contentPlaced){
+                Coordinates coordinates = worldManager.selectRandomCoordinates();
+                if (worldManager.isEmpty(coordinates)) {
+                    contentPlaced = worldManager.registerBeing(coordinates, pimpki.iterator().next());
+                }
+            }
+        }
         return true;
     }
 
